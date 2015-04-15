24f88ebbdbce06cf8517849b807860f9cce952c1
chunyou0830 <chunyou0830@gmail.com>
2015-04-15 20:12:42 +0800

    6-0 Finish

--- Lab6_0/keypad_test.v
+++ Lab6_0/keypad_test.v
@@ -1,25 +1,12 @@
 `timescale 1ns / 1ps
 
 ////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   16:29:37 04/15/2015
-// Design Name:   keypad
-// Module Name:   D:/Programming/EE2230/Lab6_0/keypad_test.v
-// Project Name:  Lab6_0
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: keypad
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Company: NTHUEE
+// Engineer: Chun You, Yang
+// Create Date: 16:29 04/15/2015
+// Design Name: keypad
+// Module Name: D:/Programming/EE2230/Lab6_0/keypad_test.v
+// Project Name: Lab6_0
 ////////////////////////////////////////////////////////////////////////////////
 
 module keypad_test;
@@ -46,23 +33,20 @@ module keypad_test;
 		// Initialize Inputs
 		clk = 1;
 		rst_n = 0;
-		col_in = 1111;
-
-		// Wait 100 ns for global reset to finish
-		#200 rst_n=1;
-		
+		col_in = 4'b1111;
 		
 		// Add stimulus here
-		#200 col_in=4'b0111;
-		#200 col_in=4'b1011;
-		#200 col_in=4'b1101;
-		#200 col_in=4'b1110;
+		#1 rst_n = 1; col_in = 4'b0111;
+		#200 col_in = 4'b1011;
+		#200 col_in = 4'b1101;
+		#200 col_in = 4'b1110;
+		#200 col_in = 4'b1111;
 		
 	end
 	
-	always
+	always 
 	begin
-		#10 clk=~clk;
+		#1 clk=~clk;
 	end
       
 endmodule
