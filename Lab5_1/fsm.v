654fd9bb0cf58740e292f016ca8bdf50a82b0644
Chunyou Yang <chunyou0830@gmail.com>
2015-04-19 22:04:18 +0800

    Clean up project files

--- Lab5_1/fsm.v
+++ Lab5_1/fsm.v
@@ -1,35 +1,18 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: NTHUEE
-// Engineer: Chun You, Yang
-// 
-// Create Date:    16:19:52 04/09/2015 
-// Design Name: 
-// Module Name:    fsm 
-// Project Name: Lab5_1
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Company		: NTHUEE
+// Engineer		: Chun You, Yang
+// Create Date	: 16:19 04/09/2015 
+// Module Name	: fsm 
+// Project Name	: Lab5_2
 //////////////////////////////////////////////////////////////////////////////////
-`define STAT_PAUSE 1'b0;
-`define STAT_COUNT 1'b1;
-`define CNT_DIS 1'b0;
-`define CNT_EN 1'b1;
-`define RST_EN 1'b0;
-`define RST_DIS 1'b1;
+`include "global.v"
 module fsm(
 	count_enable,
 	in,
 	rst_n,
 	clk
-    );
+);
 
 	output reg count_enable;
 	input in;
