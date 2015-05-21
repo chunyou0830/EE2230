a16932e317d5424b5b833db6638192eb35306bd7
chunyou0830 <chunyou0830@gmail.com>
2015-05-21 14:11:01 +0800

    10-1 Refresh

--- Lab10_1/sound_setting.v
+++ Lab10_1/sound_setting.v
@@ -14,25 +14,43 @@ module sound_setting(
 	input clk, clk_cnt;
 	input rst;
 	output reg [19:0] note_data;
-	reg [6:0] note_choose;
+	reg [15:0] note_choose;
 
 	// Note Control
 	always @*
 	begin
-		if(note_choose[0])			// Do
+		if(note_choose[0])			// L La
+			note_data = 20'd90909;
+		else if(note_choose[1])		// L Si
+			note_data = 20'd81633;
+		else if(note_choose[2])		// M Do
 			note_data = 20'd76628;
-		else if (note_choose[1])	// Re
+		else if(note_choose[3])		// M Re
 			note_data = 20'd68259;
-		else if(note_choose[2])		// Mi
+		else if(note_choose[4])		// M Mi
 			note_data = 20'd60606;
-		else if(note_choose[3])		// Fa
-			note_data = 20'd57306;
-		else if(note_choose[4])		// Sol
+		else if(note_choose[5])		// M Fa
+			note_data = 20'd57307;
+		else if(note_choose[6])		// M Sol
 			note_data = 20'd51020;
-		else if(note_choose[5])		// La
-			note_data = 20'd45454;
-		else if(note_choose[6])		// Si
-			note_data = 20'd40485;
+		else if(note_choose[7])		// M La
+			note_data = 20'd45455;
+		else if(note_choose[8])		// M Si
+			note_data = 20'd40486;
+		else if(note_choose[9])		// H Do
+			note_data = 20'd38168;
+		else if(note_choose[10])	// H Re
+			note_data = 20'd34014;
+		else if(note_choose[11])	// H Mi
+			note_data = 20'd30303;
+		else if(note_choose[12])	// H Fa
+			note_data = 20'd28653;
+		else if(note_choose[13])	// H Sol
+			note_data = 20'd25510;
+		else if(note_choose[14])	// H La
+			note_data = 20'd22727;
+		else if(note_choose[15])	// H Si
+			note_data = 20'd20243;
 		else
 			note_data = 20'd0;
 	end
@@ -42,7 +60,7 @@ module sound_setting(
 		if (rst)
 		begin
 			note_choose[0]<=1;
-			note_choose[6:1]<=6'b000000;
+			note_choose[15:1]<=15'b000000000000000;
 		end
 		else
 		begin
@@ -52,7 +70,16 @@ module sound_setting(
 			note_choose[4]<=note_choose[3];
 			note_choose[5]<=note_choose[4];
 			note_choose[6]<=note_choose[5];
-			note_choose[0]<=note_choose[6];
+			note_choose[7]<=note_choose[6];
+			note_choose[8]<=note_choose[7];
+			note_choose[9]<=note_choose[8];
+			note_choose[10]<=note_choose[9];
+			note_choose[11]<=note_choose[10];
+			note_choose[12]<=note_choose[11];
+			note_choose[13]<=note_choose[12];
+			note_choose[14]<=note_choose[13];
+			note_choose[15]<=note_choose[14];
+			note_choose[0]<=note_choose[15];
 		end
 	end
 endmodule
